38e1d3a28722834ddaf00bf2b9e82e6745d610b6
Guinryuu <kilipl.astrape@gmail.com>
2016-04-15 14:22:58 -0500

    Patrón Template Method V1

---
+++ PatronTemplateMethod/MotorGasolina.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronTemplateMethod
+{
+    public class MotorGasolina : Motor
+    {
+
+        protected override void BajarPiston()
+        {
+            Console.WriteLine("Inyectando aire y combustible en el motor");
+        }
+
+        protected override void ConsumirCombustible()
+        {
+            Console.WriteLine("COMENZANDO FASE DE EXPLOSIÓN");
+
+            Console.WriteLine("Iniciando chispa en la bujía");
+            Console.WriteLine("La explosión provoca el movimiento del pistón");
+
+            anguloCiguenal = SumarAngulo(anguloCiguenal, 180);
+            anguloArbolLevas = SumarAngulo(anguloArbolLevas, 90);
+
+            Console.WriteLine("Angulo del ciguenal: " + anguloCiguenal);
+            Console.WriteLine("Angulo del arbol de levas: " + anguloArbolLevas + "\n");
+        }
+
+    }
+}
---
+++ PatronTemplateMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronTemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Motor mGasolina = new MotorGasolina();
+            Motor mDiesel = new MotorDiesel(true);
+
+            mGasolina.RealizarFaseMotor();
+            Console.WriteLine("----------------------------------------------------");
+            mDiesel.RealizarFaseMotor();
+
+
+            
+            //Pausa la ejecución para visualizar resultados
+            Console.Read();
+
+        }
+    }
+}
