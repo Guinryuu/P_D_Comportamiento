d5c66387410c719c377c54ac1cb40cb730ab94d3
Guinryuu <kilipl.astrape@gmail.com>
2016-04-15 16:58:35 -0500

    Patrón Observer V1

---
+++ PatronObserver/ISubject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronObserver
+{
+    public interface ISubject
+    {
+
+        // Interfaz que expone los metodos de registro y eliminacion de observers, asi como para
+        // la notificacion de los cambios de estado.
+        void RegistrarObserver(IObserver o);
+        void EliminarObserver(IObserver o);
+        void NotificarObservers();
+
+    }
+}
