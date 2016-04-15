2d003fcb8838a5aab50458c5decc78d79ba1039c
Guinryuu <kilipl.astrape@gmail.com>
2016-04-15 15:50:38 -0500

    Patrón Strategy V1

---
+++ PatronStrategy/ConduccionNormal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronStrategy
+{
+    public class ConduccionNormal : ITipoConduccion
+    {
+
+        public string ObtenerDescripcion()
+        {
+            return "Conduccion Normal";
+        }
+
+        public int ObtenerPotencia(float decilitrosCombustible)
+        {
+            return (int)(decilitrosCombustible * 0.842) + 3;
+        }
+
+        public int ObtenerIncrementoVelocidad(float decilitrosCombustible)
+        {
+            return (int)(decilitrosCombustible * 0.422) + 2;
+        }
+
+    }
+}
---
+++ PatronStrategy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronStrategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Vehiculo v = new Vehiculo();
+            v.ConduccionDeportiva();
+            v.Acelerar(2.4f);
+
+            Console.WriteLine("");
+
+            v.ConduccionNormal();
+            v.Acelerar(2.4f);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
